87e42726e063eb1cfab1d956b914337b80e9a8c5
Kasey Turner <turnerkasey0@gmail.com>
2025-09-10 03:08:09 -0400

    Fix base path for local development vs production

---
+++ bloomtrack-app/vite.config-Kc.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/bloomtrackFinal/' : '/',
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly'
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+})
--- bloomtrack-app/vite.config.js
+++ bloomtrack-app/vite.config.js
@@ -3,19 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bloomtrackFinal/',
   css: {
     modules: {
       localsConvention: 'camelCaseOnly'
     }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
+  }
 })
